52d7e9bb08edfdd0d7345efcd76818ddda828e9b
Ritty Thomas <37534943+rittythomas@users.noreply.github.com>
2019-01-29 19:02:38 +0530

    Add files via upload

---
+++ Javascript/Part3.js
@@ -0,0 +1,5 @@
+alert("Welcome to a weight metric converter")
+var lbs = prompt("Enter weight in pounds:")
+var kilo = lbs * .454
+alert("Weight in Kilogram :"+ kilo)
+console.log("Conversion complete.")
